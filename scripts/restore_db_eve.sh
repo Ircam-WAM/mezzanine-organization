0189c3bb06f356ebff26060becefcb883d26764f
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-04-07 13:40:53 +0200

    update DB and media

--- scripts/restore_db_eve.sh
+++ scripts/restore_db_eve.sh
@@ -4,11 +4,11 @@ export PGPASSWORD="HmazS2frT"
 
 db_exists=`psql -hpgdb -Ueve -lqt  | cut -d \| -f 1 | grep -w eve | wc -l`
 
-if [ ! $db_exists == 0 ]; then
-    psql -hpgdb -Ueve  -c 'drop database eve'
-fi
+#if [ ! $db_exists == 0 ]; then
+#    psql -hpgdb -Ueve  -c 'drop database eve'
+#fi
 
 psql -hpgdb -Ueve -c 'create role eve'
 psql -hpgdb -Ueve -c 'create role django'
-psql -hpgdb -Ueve -c 'create database eve'
+#psql -hpgdb -Ueve -c 'create database eve'
 gunzip -c /srv/backup/eve.sql.gz | psql -hpgdb -Ueve -q eve
