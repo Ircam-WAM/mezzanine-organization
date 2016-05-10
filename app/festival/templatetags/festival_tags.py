9c4e4ba9327f9e90e20b6b59186ca13654822f3f
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-05-10 16:55:57 +0200

    conditional breaking news

--- app/festival/templatetags/festival_tags.py
+++ app/festival/templatetags/festival_tags.py
@@ -32,4 +32,7 @@ def featured(*args):
 
 @register.as_tag
 def featured_breaking_news_content(*args):
-    return Featured.objects.get(id=settings.BREAKING_NEWS_FEATURED_ID).pages.all()[0].richtextpage.content
+    news = Featured.objects.get(id=settings.BREAKING_NEWS_FEATURED_ID).pages.all()
+    if news:
+        return news[0].richtextpage.content
+    return ''
