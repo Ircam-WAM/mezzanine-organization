b30f4765089fc96c9642ff044fe5fa26e599c666
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-05-24 18:19:01 +0200

    fix empty DB

--- festival/templatetags/festival_tags.py
+++ festival/templatetags/festival_tags.py
@@ -24,28 +24,38 @@ def featured_edito(*args):
 
 @register.as_tag
 def featured_events(*args):
-    return Featured.objects.all()[0].events.order_by('start')
+    featured = Featured.objects.all()
+    if featured:
+        return featured[0].events.order_by('start')
+    return None
 
 @register.as_tag
 def featured(*args):
-    featured = Featured.objects.get(id=settings.HOME_FEATURED_ID)
     featured_list = []
-    for post in featured.blogposts.all():
-        featured_list.append(post)
-    for video in featured.videos.all():
-        featured_list.append(video)
-    for artist in featured.artists.all():
-        featured_list.append(artist)
-    for playlist in featured.playlists.all():
-        featured_list.append(playlist)
-    shuffle(featured_list)
+    featured = Featured.objects.filter(id=settings.HOME_FEATURED_ID)
+    if featured:
+        featured = featured[0]
+        for post in featured.blogposts.all():
+            featured_list.append(post)
+        for video in featured.videos.all():
+            featured_list.append(video)
+        for artist in featured.artists.all():
+            featured_list.append(artist)
+        for playlist in featured.playlists.all():
+            featured_list.append(playlist)
+        shuffle(featured_list)
     return featured_list
 
 @register.as_tag
 def featured_breaking_news_content(*args):
-    news = Featured.objects.get(id=settings.BREAKING_NEWS_FEATURED_ID).pages.all()
-    if news:
-        return news[0].richtextpage.content
+    featured = Featured.objects.filter(id=settings.BREAKING_NEWS_FEATURED_ID)
+    if featured:
+        featured = featured[0]
+        news = featured.pages.all()
+        if news:
+            return news[0].richtextpage.content
+        else:
+            return ''
     return ''
 
 @register.filter
