b4a1fffe48f2bef4bb21c957702b090c994dab57
Emilie <zawadzki@ircam.fr>
2016-07-05 18:28:35 +0200

    Task #59: Basic Page - adding image

--- app/custom/translations.py
+++ app/custom/translations.py
@@ -1,24 +1,3 @@
-# from modeltranslation.translator import translator, register, TranslationOptions
-# from mezzanine.pages.models import Page, RichTextPage, Link
-# from mezzanine.pages.translation import TranslatedRichText
-# from mezzanine.forms.models import Form
-# from mezzanine.galleries.models import Gallery
-#
-# # @register(RichText)
-# # class RichTextTranslationOptions(TranslatedRichText):
-# #
-# #     fields = ('sub_title')
-#
-# # print(RichTextPage._meta.get_fields())
-# # print('///////////////////////////')
-# class TranslatedInjectedRichText(TranslatedRichText):
-#     fields = ('sub_title',)
-#
-# translator.unregister(RichTextPage)
-#
-# translator.register(RichTextPage, TranslatedInjectedRichText)
-
-
 from modeltranslation.translator import translator, register, TranslationOptions
 from mezzanine.pages.models import Page, RichText
 from mezzanine.pages.translation import TranslatedRichText
