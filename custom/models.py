1182ca57e05d1ea026a8064214e64e8b6104fc39
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-07-06 17:51:12 +0200

    add media model and Photos bundle

--- custom/models.py
+++ custom/models.py
@@ -5,24 +5,21 @@ from mezzanine.pages.models import Page, RichText
 from mezzanine.core.fields import RichTextField, OrderField, FileField
 from django.conf import settings
 
+from media.models import Photos
+
 ALIGNMENT_CHOICES = (('left', _('left')), ('right', _('right')))
 MEDIA_BASE_URL = getattr(settings, 'MEDIA_BASE_URL', 'http://medias.ircam.fr/embed/media/')
 
+
 class SubTitle(models.Model):
 
-    sub_title = models.TextField(_('sub title'), blank=True)
+    sub_title = models.TextField(_('sub title'), blank=True, max_length=1024)
 
     class Meta:
         abstract = True
 
 
-class BasicPage(Page, RichText):
+class BasicPage(Page, RichText, SubTitle, Photos):
 
-    sub_title = models.CharField(_('sub title'), blank=True, max_length=1000)
-    # description = models.TextField(_('description'), blank=True)
-    photo = FileField(_('photo'), upload_to='images/photos', max_length=1024, blank=True, format="Image")
-    photo_credits = models.CharField(_('photo credits'), max_length=255, blank=True, null=True)
-    photo_alignment = models.CharField(_('photo alignment'), choices=ALIGNMENT_CHOICES, max_length=32, default="left", blank=True)
-    photo_description = models.TextField(_('photo description'), blank=True)
-    photo_featured = FileField(_('photo featured'), upload_to='images/photos', max_length=1024, blank=True, format="Image")
-    photo_featured_credits = models.CharField(_('photo featured credits'), max_length=255, blank=True, null=True)
+    class Meta:
+        verbose_name = 'basic page'
