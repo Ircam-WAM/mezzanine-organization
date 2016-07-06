74ed02e43827a5f873b810f18a17e81611572c4b
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-07-06 11:59:31 +0200

    add debug-toolbar and more extensions (needs rebuild)

--- app/local_settings.py
+++ app/local_settings.py
@@ -117,3 +117,23 @@ BREAKING_NEWS_FEATURED_ID = 4
 BLOG_POST_PER_PAGE = 200
 
 FILEBROWSER_MAX_UPLOAD_SIZE = 512000000
+
+if DEBUG:
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': lambda x : True
+    }
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
