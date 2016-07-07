7e5071dd69e9d12e3eb4c49e0bf8731fd36b20f9
Emilie <zawadzki@ircam.fr>
2016-07-07 17:51:09 +0200

    deleting autocompletion

--- organization/magazine/views.py
+++ organization/magazine/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import *
+from django.views.generic.base import *
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class BriefDetailView(SlugMixin, DetailView):
+
+    model = Brief
+    template_name='magazine/inc_brief.html'
+    context_object_name = 'brief'
+
+    def get_context_data(self, **kwargs):
+        context = super(BriefDetailView, self).get_context_data(**kwargs)
+        return context
