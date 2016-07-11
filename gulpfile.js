ff78c17e0f93aec68ad0d70be60a25d63024c015
Jérémy Fabre <blackmagik88@gmail.com>
2016-07-11 16:38:52 +0200

    Gulpfile update and static folder cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,138 @@
-/**
- * Plugins
- */
-var gulp         = require('gulp');
-    sass         = require('gulp-sass'),
-    postcss      = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    mqpacker     = require('css-mqpacker'),
-    plumber      = require('gulp-plumber'),
-    concat       = require('gulp-concat'),
-    notify       = require("gulp-notify"),
-    uglify       = require('gulp-uglify');
+var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    compass = require('gulp-compass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    ignore = require('gulp-ignore'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create(),
+    runSequence = require('run-sequence'),
+    imagemin = require('gulp-imagemin')
+    favicons = require('gulp-favicons'),
+    gutil = require('gulp-util'),
+    copy = require('gulp-copy'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserify = require('gulp-browserify'),
+    sourcemaps = require('gulp-sourcemaps');
 
+var srcFolder = 'app/static/src/',
+    destFolder = 'app/static/'
 
-
-/**
- * Paths
- */
-var scssSrc = 'app/static/scss/',
-    jsSrc   = 'app/static/js/',
-    cssDist = 'app/static/css/',
-    cssDjango = 'data/static/css/';
-
-
-/**
- * Environnement
- */
-
-env = (function() {
-    var env = 'development';
-    return env;
-} ());
-
-// Set to production (for builds)
-gulp.task( 'envProduction', function() {
-    env = 'production';
+gulp.task('copy-assets-img', function() {
+    gulp.src([srcFolder + 'assets/img/**/*'])
+        .pipe(gulp.dest(destFolder + 'img'));
 });
 
+gulp.task("favicons", function () {
+    return gulp.src(srcFolder + "assets/favicon/favicon.png")
+        .pipe(favicons({
+            background: "#ffffff",
+            path: "/favicons/",
+            replace: true,
+            icons: {
+                android: false,              // Create Android homescreen icon. `boolean`
+                appleIcon: true,            // Create Apple touch icons. `boolean`
+                appleStartup: false,         // Create Apple startup images. `boolean`
+                coast: false,                // Create Opera Coast icon. `boolean`
+                favicons: true,             // Create regular favicons. `boolean`
+                firefox: false,              // Create Firefox OS icons. `boolean`
+                opengraph: false,            // Create Facebook OpenGraph image. `boolean`
+                twitter: false,              // Create Twitter Summary Card image. `boolean`
+                windows: false,              // Create Windows 8 tile icons. `boolean`
+                yandex: false                // Create Yandex browser icon. `boolean`
+            }
+        }))
+        .on("error", gutil.log)
+        .pipe(gulp.dest(destFolder + '/img/favicons/'));
+});
 
-/**
- * CSS
- */
-gulp.task('css', function () {
+gulp.task('main-js', function() {
+    return gulp.src(srcFolder + 'js/index.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }})
+        )
+        .pipe(browserify({
+		  insertGlobals : true,
+		  debug : false
+		}))
+        .pipe(rename('index.min.js'))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(destFolder + 'js'))
+        .pipe(browserSync.stream());
+});
 
-    if ( env === 'production' ) {
-        output = 'compressed';
-    } else {
-        output = 'expanded';
-    }
+gulp.task('main-css', function() {
+    return gulp.src(srcFolder + 'sass/*.scss')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                this.emit('end');
+            }})
+        )
+        .pipe(compass({
+            css: './.tmp/main',
+            sass: srcFolder + 'sass'
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(destFolder + 'css'))
+        .pipe(browserSync.stream());
+});
 
-    var processors = [
-        autoprefixer({browsers: ['last 2 version']}),
-        mqpacker({
-            sort: true
-        })
-    ];
+gulp.task('cssmin', function() {
+    return gulp.src(destFolder + 'css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cssnano())
+        .on("error", gutil.log)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(destFolder + 'css/'))
+        .pipe(browserSync.stream());
+});
 
-    return gulp.src( scssSrc + 'index.scss'  )
-        .pipe(sass({
-            outputStyle : output,
-            sourceComments: 'normal'
-        })
-        .on('error', notify.onError("Error: <%= error.message %>")))
-        .pipe(postcss(processors))
-        .pipe(gulp.dest(cssDist))
-        .pipe(gulp.dest(cssDist));
+gulp.task('jsmin', function() {
+    return gulp.src(destFolder + 'js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(uglify({'mangle': false}))
+        .on("error", gutil.log)
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(destFolder + 'js/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src(destFolder + 'img/**/*')
+		.pipe(imagemin({
+			progressive: true,
+            optimizationLevel: 3
+		}))
+        .on("error", gutil.log)
+		.pipe(gulp.dest(destFolder + 'img'));
+});
+
+gulp.task('clean', function(cb) {
+    rimraf('.tmp', cb);
+});
+
+gulp.task('serve', ['clean'], function () {
+
+    browserSync.init({
+        proxy: "http://localhost:9020/"
+    });
+
+    gulp.watch(srcFolder + 'assets/img/**/*', ['copy-assets-img']).on('change', browserSync.reload);
+	gulp.watch(srcFolder + 'js/**/*.js', ['main-js']);
+	gulp.watch(srcFolder + 'sass/**/*.scss', ['main-css']);
 
 });
 
-/**
- * JAVASCRIPT
- */
-
-// Concatenate all JS libs
-gulp.task('jsLibs', function() {
-    console.log(jsSrc);
-    return gulp.src(jsSrc + 'plugins/*.js')
-    .pipe(concat('plugins.js'))
-    .pipe(gulp.dest(jsSrc));
-});
-
-// Move main js script file
-// gulp.task('jsScripts', function() {
-//   gulp.src(jsSrc + 'index.js')
-//     .pipe(plumber())
-//     .pipe(gulp.dest(dist + 'js/'));
-// });
-
-// Move and minify main js script file
-// gulp.task('jsScriptsBuild', function() {
-//   gulp.src('src/js/index.js')
-//     .pipe(plumber())
-//     .pipe(uglify())
-//     .pipe(gulp.dest(dist + 'js/'));
-// });
-
-
- /**
- * TASKS
- */
-
-// default task (development)
-gulp.task('default', ['css', 'jsLibs'], function () {
-    gulp.watch( scssSrc + '**/*.scss', ['css']);
-    gulp.watch( jsSrc + 'plugins/*.js', ['jsLibs']);
-});
-
-// Build tasks
-gulp.task( "build", [ 'envProduction', 'css'], function () {
-    console.log("Build complete !");
+gulp.task('default', ['main-js', 'main-css', 'copy-assets-img', 'serve']);
+gulp.task('build', ['main-js', 'main-css', 'copy-assets-img'], function() {
+    runSequence(['cssmin', 'jsmin', 'imagemin', 'favicons', 'clean']);
 });
