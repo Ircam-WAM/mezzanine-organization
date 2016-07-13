48e05d7149d021c192afa3553535f4f5ba90973f
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-07-13 15:51:21 +0200

    add fields, translations, try to fix admin menus

--- app/organization/project/admin.py
+++ app/organization/project/admin.py
@@ -1,25 +1,21 @@
-from django.contrib import admin
-from mezzanine.blog.admin import BlogPostAdmin
-from organization.magazine.models import Article
-#from custom.admin import SubTitleAdmin
 from copy import deepcopy
+from django.contrib import admin
+from organization.project.models import Project
+#from custom.admin import SubTitleAdmin
 
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from mezzanine.core.admin import DisplayableAdmin, OwnableAdmin
 
-# class ArticleAdmin(BlogPostAdmin, SubTitleAdmin):
-#
-#     model = Article
 
-#admin.site.register(Article, BlogPostAdmin)
-class ArticleAdmin(admin.ModelAdmin):
+class ProjectAdmin(admin.ModelAdmin):
 
-    model = Article
+    model = Project
 
 
-class ArticleAdminDisplayable(DisplayableAdmin):
+class ProjectAdminDisplayable(DisplayableAdmin):
 
-    fieldsets = deepcopy(ArticleAdmin.fieldsets)
+    fieldsets = deepcopy(ProjectAdmin.fieldsets)
 
-admin.site.register(Article, ArticleAdminDisplayable)
+
+admin.site.register(Project, ProjectAdminDisplayable)
