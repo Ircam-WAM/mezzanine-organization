3fbee5b0c5f4376af1c855137f6c586c9f661719
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-07-13 15:51:21 +0200

    add fields, translations, try to fix admin menus

--- organization/team/admin.py
+++ organization/team/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django import forms
+from copy import deepcopy
+from mezzanine.core.admin import DisplayableAdmin, BaseDynamicInlineAdmin, BaseTranslationModelAdmin
+from organization.team.models import *
 
-# Register your models here.
+
+
+class ActivityInline(admin.StackedInline):
+
+    model = Activity
+    extras = 3
+
+
+class PersonAdmin(admin.ModelAdmin):
+
+    model = Person
+
+
+class PersonAdminDisplayable(DisplayableAdmin):
+
+    fieldsets = deepcopy(PersonAdmin.fieldsets)
+    inlines = [ActivityInline,]
+
+
+admin.site.register(Organization)
+admin.site.register(OrganizationType)
+admin.site.register(Department)
+admin.site.register(Team)
+admin.site.register(Person, PersonAdminDisplayable)
+admin.site.register(Activity)
