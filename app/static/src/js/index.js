520dc1388ecb1a1a3add16a1dc4724b808713eeb
Jérémy Fabre <blackmagik88@gmail.com>
2016-07-20 14:11:53 +0200

    Header menu with submenu

--- app/static/src/js/index.js
+++ app/static/src/js/index.js
@@ -2,6 +2,7 @@
 // Require all the modules
 //
 var LangSelector = require('./modules/lang-selector');
+var NavHeader = require('./modules/nav-header');
 var OpenButton = require('./modules/open-button');
 var CloseButton = require('./modules/close-button');
 var CloseEscape = require('./modules/close-escape');
@@ -11,6 +12,7 @@ var Search = require('./modules/search');
 // Init all the modules
 //
 window[LangSelector] = new LangSelector();
+window[NavHeader] = new NavHeader();
 window[OpenButton] = new OpenButton();
 window[CloseButton] = new CloseButton();
 window[CloseEscape] = new CloseEscape();
