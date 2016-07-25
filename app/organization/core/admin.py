dc9cc2c5206102e615d8a7d20080aa0591be5cf1
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-07-25 02:01:50 +0200

    add page blocks to team pages, add various translations

--- app/organization/core/admin.py
+++ app/organization/core/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from copy import deepcopy
-from mezzanine.pages.models import Page
+from mezzanine.core.admin import *
 from mezzanine.pages.admin import PageAdmin
-from mezzanine.pages.models import RichTextPage
-from organization.core.models import BasicPage
+from organization.core.models import *
+
+
+class PageBlockInline(StackedDynamicInlineAdmin):
+
+    model = PageBlock
 
 
 admin.site.register(BasicPage, PageAdmin)
