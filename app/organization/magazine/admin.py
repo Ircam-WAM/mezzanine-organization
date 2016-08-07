2abf95af62d67329a11ce17ccf6e118cb87bd1ed
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-08-07 17:38:18 +0200

    Fix stacked inlines thanks to a special grapelli branch, add various admin filters

--- app/organization/magazine/admin.py
+++ app/organization/magazine/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
 from django import forms
 from copy import deepcopy
-from mezzanine.core.admin import DisplayableAdmin
+from mezzanine.core.admin import *
 from mezzanine.pages.admin import PageAdmin
 from orderable.admin import OrderableAdmin, OrderableTabularInline
 from organization.magazine.models import Article, Brief ,Topic
 
 
 class ArticleAdmin(admin.ModelAdmin):
+
     model = Article
+
 
 class ArticleAdminDisplayable(DisplayableAdmin):
 
     fieldsets = deepcopy(ArticleAdmin.fieldsets)
     exclude = ('related_posts',)
+    filter_horizontal = ['categories', 'related_articles', ]
+
 
 class BriefAdmin(OrderableTabularInline):
 
     model = Brief
 
-class BriefAdminDisplayable(OrderableAdmin):
+
+class BriefAdminDisplayable(BaseTranslationModelAdmin, OrderableAdmin):
 
     list_display = ('title', 'local_content', 'sort_order_display',)
     fieldsets = deepcopy(BriefAdmin.fieldsets)
