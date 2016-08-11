361726c380dcbab5bd70f7e7616ca62c867dc186
Emilie <zawadzki@ircam.fr>
2016-08-11 10:47:41 +0200

    Breadcrumb for article, depends from where user is coming. Department or Topic.

--- app/organization/magazine/views.py
+++ app/organization/magazine/views.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from django.shortcuts import render
 from django.utils import timezone
 #from django.views.generic import *
@@ -5,7 +6,9 @@ from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.base import *
 from django.shortcuts import get_object_or_404
 
-from organization.magazine.models import Article, Topic ,Brief
+from organization.magazine.models import Article, Topic, Brief
+from organization.team.models import Department
+
 from organization.core.views import SlugMixin
 from django.template.defaultfilters import slugify
 
@@ -16,8 +19,22 @@ class ArticleDetailView(SlugMixin, DetailView):
     template_name='magazine/article/article_detail.html'
     context_object_name = 'article'
 
+    def get(self, request, *args, **kwargs):
+        previous_page_url = request.META['HTTP_REFERER']
+        previous_page_slug = request.META['HTTP_REFERER'].rsplit("/")[-2]
+        if previous_page_slug:
+            #find parents page
+            parsed_url = urlparse(previous_page_url)
+            self.department_parent = Department.objects.filter(slug=parsed_url.path[1:][:-1])
+            self.topic_parent = Topic.objects.filter(slug=previous_page_slug)
+        return super(ArticleDetailView, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        if self.topic_parent:
+            context['topic_parent'] = self.topic_parent.all()[0]
+        if self.department_parent:
+            context['department_parent'] =  self.department_parent.all()[0]
         return context
 
 
