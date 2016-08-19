fca16a4e26967f5c13bcf9d32c0661ad14247c4c
Emilie <zawadzki@ircam.fr>
2016-08-19 18:01:03 +0200

    Home : slider + repair debug bar tool

--- organization/featured/views.py
+++ organization/featured/views.py
@@ -2,16 +2,3 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView
 from organization.core.views import SlugMixin
 from organization.magazine.models import Brief
-
-class HomeView(SlugMixin, TemplateView):
-
-    template_name = 'index.html'
-    briefs = Brief.objects.all() # with .published, order by isn't working anymore
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['briefs'] = self.briefs
-        return context
-
-    def get_queryset(self, **kwargs):
-        return self.model.objects.published()
