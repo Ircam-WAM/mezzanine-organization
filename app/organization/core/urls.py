03cbbf9d62ba0cd564552d1a8ad8f8c14e61ddae
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-08-29 17:36:32 +0200

    Add custom displayble url

--- app/organization/core/urls.py
+++ app/organization/core/urls.py
@@ -7,5 +7,8 @@ from django.conf.urls.i18n import i18n_patterns
 from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 
+from organization.core.views import *
+
 urlpatterns = [
+    url(r'^displayable/(?P<slug>.*)/$', CustomDisplayableView.as_view(), name="organization-displayable"),
 ]
--- app/organization/core/views.py
+++ app/organization/core/views.py
@@ -3,8 +3,16 @@ from django.http import Http404
 from django.views.generic.base import View
 from django.views.generic import DetailView, ListView, TemplateView
 
+from organization.core.models import *
+
+
 class SlugMixin(object):
 
     def get_object(self):
         objects = self.model.objects.all()
         return get_object_or_404(objects, slug=self.kwargs['slug'])
+
+
+class CustomDisplayableView(SlugMixin, DetailView):
+
+    model = CustomDisplayable
