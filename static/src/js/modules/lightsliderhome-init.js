557634697da7cb9e3e09df9d0df230ca8b6e51d1
Jérémy Fabre <blackmagik88@gmail.com>
2016-08-31 16:45:04 +0200

    Add the slider on the homepage

---
+++ static/src/js/modules/lightsliderhome-init.js
@@ -0,0 +1,42 @@
+var LightSliderHomeInit = function() {
+
+    //
+    // Init
+    //
+    this.init();
+
+};
+
+LightSliderHomeInit.prototype.init = function() {
+
+    var that = this;
+
+    $('[data-slider-home]').lightSlider({
+        item: 1,
+        slideMargin: 0,
+        pager: false,
+        onAfterSlide: function(slider) {
+            var $pages = slider.parents('.slider-home').find('.slider-home__pager li');
+            $pages.removeClass('active');
+            console.log(slider.getCurrentSlideCount());
+            $($pages[slider.getCurrentSlideCount()-1]).addClass('active');
+        },
+        onSliderLoad: function(slider) {
+            var $pages = slider.parents('.slider-home').find('.slider-home__pager li');
+            $pages.each(function(idx) {
+                var that = $(this);
+                that.click(function(e) {
+                    e.preventDefault();
+                    $pages.removeClass('active');
+                    that.addClass('active');
+                    slider.goToSlide(idx);
+                    return false;
+                });
+
+            });
+        }
+    });
+
+};
+
+module.exports = LightSliderHomeInit;
