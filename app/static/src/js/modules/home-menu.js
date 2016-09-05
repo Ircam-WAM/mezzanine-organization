e42481314e093c38a39db589a50db938b8bfeab2
Jérémy Fabre <blackmagik88@gmail.com>
2016-09-05 16:55:55 +0200

    Add the topic and brief list on the homepage shutters

--- app/static/src/js/modules/home-menu.js
+++ app/static/src/js/modules/home-menu.js
@@ -5,20 +5,37 @@ var HomeMenu = function() {
     //
     this.init();
 
+    this.$menu = $('.home__menu-item');
+
 };
 
 HomeMenu.prototype.init = function() {
 
     var that = this,
-        $elements = $('.home__menu a');
+        $elements = $('.home__menu-item a');
 
-    $elements.hover(function() {
+    $('.home__menu-item').hover(function() {
+
         $elements.removeClass('active');
-        $(this).addClass('active');
+        $(this).find('>a').addClass('active');
+        $('.home__shutter').removeClass('active');
+        $(this).find('.home__shutter').addClass('active');
+
     }, function() {
+
+        $('.home__shutter').removeClass('active');
         $elements.removeClass('active');
         $($elements.get(0)).addClass('active');
+
     });
+
+    /*$('.home__menu').bind('mouseleave', function() {
+
+        $('.home__shutter').removeClass('active');
+        $elements.removeClass('active');
+        $($elements.get(0)).addClass('active');
+
+    });*/
 
 };
 
