a7c64ff558ac23e8a628f8d6f8d307297e3420b2
Emilie <zawadzki@ircam.fr>
2016-09-09 11:51:06 +0200

    JobOffer : check extension file

--- app/organization/job/views.py
+++ app/organization/job/views.py
@@ -1,4 +1,6 @@
 import os
+from django import forms
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.edit import CreateView
@@ -8,6 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 from organization.job.models import JobOffer, JobResponse
 from organization.job.forms import JobResponseForm
 
+extention = ['.pdf', '.PDF', '.doc', '.docx']
 
 class JobOfferDetailView(CreateView):
 
@@ -35,10 +38,11 @@ class JobOfferDetailView(CreateView):
 
     def form_valid(self, form):
         # check extension uploaded files
-        # name_cv, ext_cv = os.path.splitext(self.cleaned_data['curriculum_vitae'].name)
-        # name_cl, ext_cl = os.path.splitext(self.cleaned_data['cover_letter'].name)
-        # if ext_cv not in ['.pdf', '.PDF', '.doc', '.docx']:
-        #     raise forms.ValidationError("Only .pdf, .doc, .docx files allowed")
+        name_cv, ext_cv = os.path.splitext(form.cleaned_data['curriculum_vitae'].name)
+        name_cl, ext_cl = os.path.splitext(form.cleaned_data['cover_letter'].name)
+        if ext_cv not in extention or ext_cl not in extention :
+            messages.info(self.request, _("Only .pdf, .doc, .docx files allowed."))
+            return super(JobOfferDetailView, self).form_invalid(form)
         messages.info(self.request, _("You have successfully submitted your application."))
         return super(JobOfferDetailView, self).form_valid(form)
 
