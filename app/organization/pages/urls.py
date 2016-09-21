9ad7ece203327cedc32e3f1a18dce8e506476d35
Emilie <zawadzki@ircam.fr>
2016-09-21 18:23:54 +0200

    Person List: only admin user can access autocomplete url

--- app/organization/pages/urls.py
+++ app/organization/pages/urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-
+from django.contrib.auth.decorators import permission_required
 from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 from organization.pages.views import *
@@ -12,8 +12,8 @@ _slash = "/" if settings.APPEND_SLASH else ""
 
 urlpatterns = [
     url("^$", HomeView.as_view(), name="home"),
-    url("^dynamic-content-home-slider/$", DynamicContentHomeSliderView.as_view(), name='dynamic-content-home-slider'),
-    url("^dynamic-content-home-body/$", DynamicContentHomeBodyView.as_view(), name='dynamic-content-home-body'),
+    url("^dynamic-content-home-slider/$", permission_required('home.can_edit')(DynamicContentHomeSliderView.as_view()), name='dynamic-content-home-slider'),
+    url("^dynamic-content-home-body/$",  permission_required('home.can_edit')(DynamicContentHomeBodyView.as_view()), name='dynamic-content-home-body'),
     url("^home/$", HomeView.as_view(), name='organization-home'),
 
 ]
