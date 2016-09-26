de0df0ad7df18b855881df752645e8ec4ac61d2b
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-09-26 13:31:02 +0200

    Do not update translated fields each time..

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -5,7 +5,7 @@
 git pull origin dev
 git submodule foreach git pull origin master
 docker-compose run app python /srv/app/manage.py migrate
-docker-compose run app python /srv/app/manage.py update_translation_fields
+# docker-compose run app python /srv/app/manage.py update_translation_fields
 bower install
 gulp build
 docker-compose run app python /srv/app/manage.py collectstatic --noinput
