c6900eaba8d2d7193b4e88a1888db1ebfd66b8db
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-09-27 16:08:19 +0200

    Fix no object.images

--- organization/core/templatetags/organization_tags.py
+++ organization/core/templatetags/organization_tags.py
@@ -93,15 +93,17 @@ def get_mezzanine_menu_name(menu_id):
 
 @register.filter
 def get_type(objects, type):
-    if isinstance(objects, list):
-        objs = []
-        for object in objects:
-            if obj.type == type:
-                objs.append(obj)
-    else:
-        objs = objects.filter(type=type)
-    if objs:
-        return objs
+    print(objects)
+    if objects:
+        if isinstance(objects, list):
+            objs = []
+            for object in objects:
+                if obj.type == type:
+                    objs.append(obj)
+        else:
+            objs = objects.filter(type=type)
+        if objs:
+            return objs
     return None
 
 @register.filter
