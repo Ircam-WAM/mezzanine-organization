5e143e7e1d8c88d1b9ac1c112dbc32d788599da5
Emilie <zawadzki@ircam.fr>
2016-09-29 13:05:14 +0200

    Deleting useless department and team templates

--- organization/network/urls.py
+++ organization/network/urls.py
@@ -9,9 +9,7 @@ from mezzanine.conf import settings
 
 from organization.network.views import *
 
-
 urlpatterns = [
-    url(r'^(?P<department>.*)/teams/$', TeamListView.as_view(), name="organization-network-team-list"),
     url(r'^person/(?P<slug>.*)/$', PersonDetailView.as_view(), name="organization-network-person-detail"),
     url("^person-list-block-autocomplete/$", permission_required('person.can_edit')(PersonListBlockAutocompleteView.as_view()), name='person-list-block-autocomplete'),
     url("^person-autocomplete/$", permission_required('person.can_edit')(PersonListView.as_view()), name='person-autocomplete'),
--- organization/network/views.py
+++ organization/network/views.py
@@ -5,50 +5,6 @@ from organization.network.models import *
 from organization.core.views import *
 
 
-class DepartmentListView(ListView):
-
-    model = Department
-    template_name='team/department_list.html'
-
-
-class DepartmentDetailView(SlugMixin, DetailView):
-
-    model = Department
-    template_name='team/department_detail.html'
-    context_object_name = 'department'
-
-
-class TeamListView(ListView):
-
-    model = Team
-    template_name='team/team_list.html'
-
-
-class TeamDetailView(SlugMixin, DetailView):
-
-    model = Team
-    template_name='team/team_detail.html'
-    context_object_name = 'team'
-
-    def get_context_data(self, **kwargs):
-        context = super(TeamListView, self).get_context_data(**kwargs)
-        partners = []
-
-        for partner in self.object.partner_organizations:
-            partners.append(partner)
-        for partner in self.object.partner_teams:
-            partners.append(partner)
-
-        for project in team.project_leader.all():
-            for partner in project.partner_organizations:
-                partners.append(partner)
-            for partner in project.partner_teams:
-                partners.append(partner)
-
-        context['partners'] = partners
-        return context
-
-
 class PersonListView(ListView):
 
     model = Person
