853586edc57d55cc937a42163a14cdb7d165b477
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-06 11:02:48 +0200

    Fix add products by allowing variations

--- app/settings.py
+++ app/settings.py
@@ -128,10 +128,6 @@ USE_L10N = True
 
 AUTHENTICATION_BACKENDS = ("mezzanine.core.auth_backends.MezzanineBackend",)
 
-SHOP_CURRENCY_LOCALE = ''
-SHOP_USE_VARIATIONS = False
-
-
 #############
 # DATABASES #
 #############
