0f12969f74bcd18328b7c4af8f219c36dcbfcf5d
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-11 17:41:35 +0200

    Add (temporary) audio views, DE-ACTIVATE Audio.SearchableManager (bug adding link in editor)

--- app/organization/media/urls.py
+++ app/organization/media/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     url(r'^videos/$', VideoListView.as_view(), name="festival-video-list"),
     url(r'^videos/detail/(?P<slug>.*)/$', VideoDetailView.as_view(), name="festival-video-detail"),
     url(r'^videos/category/(?P<slug>.*)/$', VideoListCategoryView.as_view(), name="festival-video-list-category"),
+    url(r'^audios/detail/(?P<slug>.*)/$', AudioDetailView.as_view(), name="festival-audio-detail"),
+    url(r'^audios/category/(?P<slug>.*)/$', AudioListCategoryView.as_view(), name="festival-audio-list-category"),
+
 ]
