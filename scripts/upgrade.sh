30a453e1881b9a5e09cce73564f24b567ce91f3a
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-14 17:19:03 +0200

    Pull local branch only

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,7 +2,7 @@
 
 # Use this script to update a dev/prod server
 
-git pull origin dev
+git pull
 git submodule foreach git pull origin master
 docker-compose run app python /srv/app/manage.py migrate
 # docker-compose run app python /srv/app/manage.py update_translation_fields
