7e325ab771b526b02de6a35685fc1563175f5500
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-18 12:30:58 +0200

    Bump postgres to 9.5, add admin person desc

--- organization/network/admin.py
+++ organization/network/admin.py
@@ -110,7 +110,7 @@ class PersonAdmin(BaseTranslationOrderedModelAdmin):
                PersonFileInline ]
     first_fields = ['last_name', 'first_name', 'title', 'gender', 'user']
     search_fields = ['last_name', 'first_name']
-    list_display = ['last_name', 'first_name', 'email', 'gender']
+    list_display = ['last_name', 'first_name', 'description', 'email', 'gender']
     list_filter = ['person_title', 'activities__date_from', 'activities__date_to', 'activities__is_permanent', 'activities__framework', 'activities__grade', 'activities__function', 'activities__team', 'activities__project',]
 
 
