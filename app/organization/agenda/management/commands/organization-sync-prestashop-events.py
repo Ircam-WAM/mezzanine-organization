cc55ea5c735471e066e37c09b1ada3badc20a6e0
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-19 18:49:33 +0200

    Fix import prestashop products to events, adapt event view

--- app/organization/agenda/management/commands/organization-sync-prestashop-events.py
+++ app/organization/agenda/management/commands/organization-sync-prestashop-events.py
@@ -9,7 +9,9 @@ from django.core.mail import EmailMessage
 from mezzanine.generic.models import AssignedKeyword, Keyword
 
 import mezzanine_agenda.models as ma_models
+import organization.agenda.models as oa_models
 import prestashop.models as pa_models
+import cartridge.shop.models as ca_models
 
 
 class Command(BaseCommand):
@@ -28,15 +30,11 @@ class Command(BaseCommand):
 
 
     def cleanup(self):
-        # for event in ma_models.Event.objects.all():
-        #     event.delete()
-        # for location in ma_models.EventLocation.objects.all():
-        #     location.delete()
-        for event_price in ma_models.EventPrice.objects.all():
-            event_price.delete()
+        for event in ma_models.Event.objects.all():
+             event.delete()
 
     def handle(self, *args, **kwargs):
-        # self.cleanup()
+        self.cleanup()
 
         products = []
         category_lang_name = kwargs.get('category_lang_name')
@@ -45,76 +43,46 @@ class Command(BaseCommand):
             for category in pa_models.PsCategoryLang.objects.all():
                 print(category.name)
 
-        print('---------------------------')
-
         category_lang_name = kwargs.get('category_lang_name')
         category_lang = pa_models.PsCategoryLang.objects.get(name=category_lang_name)
         category = pa_models.PsCategory.objects.get(id_category=category_lang.id_category)
-
         category_products = pa_models.PsCategoryProduct.objects.filter(id_category=category.id_category)
+
         for category_product in category_products:
             products.append(pa_models.PsProduct.objects.get(id_product=category_product.id_product))
 
         for product in products:
+            print('---------------------------')
             print(product.id_product)
+
+            events = ma_models.Event.objects.filter(external_id=product.id_product)
+            if events:
+                event = events[0]
+            else:
+                event = ma_models.Event(external_id=product.id_product)
+
             product_langs = pa_models.PsProductLang.objects.filter(id_product=product.id_product)
             for product_lang in product_langs:
                 lang_code = self.languages[product_lang.id_lang]['code']
 
                 for lang in self.languages:
                     if product_lang.teaching_lang.lower() in self.languages[product_lang.id_lang]['names']:
-                        teaching_lang = self.languages[product_lang.id_lang]['code']
-                        print(teaching_lang)
+                        language = self.languages[product_lang.id_lang]['code']
+                        event_training = oa_models.EventTraining(language=language)
                         break
 
-                print(product_lang.name, lang_code, product_lang.dates, product_lang.times, product_lang.description)
+                print(product_lang.name, lang_code, product_lang.dates)
 
-        # meta_trans_all = eve_models.MetaEventTranslation.objects.all()
-        # for meta_trans in meta_trans_all:
-        #     if meta_trans.name == meta_event_name:
-        #         break
-        # eve_events = eve_models.Event.objects.filter(meta_event=meta_trans.id)
-        # for eve_event in eve_events:
-        #     event_trans = eve_models.EventTranslation.objects.filter(id=eve_event, lang='fr')[0]
-        #     manifestations = eve_event.manifestations.all().order_by('happens_at')
-        #     first = True
-        #     for manifestation in manifestations:
-        #         events = ma_models.Event.objects.filter(external_id=manifestation.id)
-        #         if not events:
-        #             event = ma_models.Event(external_id=manifestation.id)
-        #         else:
-        #             event = events[0]
-        #         event.start = manifestation.happens_at
-        #         event.end = manifestation.happens_at + timedelta(seconds=manifestation.duration)
-        #         event.title = event_trans.name
-        #         event.user = self.default_user
-        #
-        #         locations = ma_models.EventLocation.objects.filter(title=manifestation.location.name)
-        #         if locations:
-        #             location = locations[0]
-        #         else:
-        #             location = ma_models.EventLocation(title=manifestation.location.name)
-        #         address = '\n'.join([manifestation.location.address, manifestation.location.postalcode + ' ' + manifestation.location.city])
-        #         location.address = address
-        #         location.external_id = manifestation.id
-        #         location.clean()
-        #         location.save()
-        #         event.location = location
-        #         event.save()
-        #         keyword, _ = Keyword.objects.get_or_create(title=eve_event.event_category.name)
-        #         event.keywords.add(AssignedKeyword(keyword=keyword), bulk=False)
-        #
-        #         eve_prices = eve_models.PriceManifestation.objects.filter(manifestation=manifestation)
-        #         for price in eve_prices:
-        #             event_price, c = ma_models.EventPrice.objects.get_or_create(value=float(price.value))
-        #             if event:
-        #                 if not event_price in event.prices.all():
-        #                     event.prices.add(event_price)
-        #
-        #         if not first:
-        #             event.parent = parent
-        #         else:
-        #             parent = event
-        #             first = False
-        #
-        #         event.save()
+                setattr(event, 'title' + '_' + lang_code, product_lang.name)
+                setattr(event, 'content' + '_' + lang_code, product_lang.description)
+                event.date_text = product_lang.dates
+
+            event.start = datetime.now()
+            event.user = self.default_user
+            event_price, c = ma_models.EventPrice.objects.get_or_create(value=product.price)
+            event.save()
+            event.prices.add(event_price)
+            event.status = 1
+            event.save()
+            event_training.event = event
+            event_training.save()
