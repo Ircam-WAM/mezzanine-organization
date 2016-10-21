fb70978c84d6402386959314ac87168cdc760f7b
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-21 10:47:50 +0200

    NO submodule init

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,6 +3,5 @@
 sudo chown -R $USER data/media
 sudo chown -R $USER data/backup
 git pull
-git submodule update --init --recursive
 git submodule foreach git pull
 docker-compose run db /srv/scripts/restore_db.sh
