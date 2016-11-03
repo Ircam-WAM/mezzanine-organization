863279713e8b189ea1a8d3536515d50a79b8db16
Emilie <zawadzki@ircam.fr>
2016-11-03 18:20:24 +0100

    Task #356: confirmation cart page

--- app/organization/agenda/urls.py
+++ app/organization/agenda/urls.py
@@ -8,8 +8,11 @@ from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 
 from organization.core.views import *
+from organization.agenda.views import *
 
 
 urlpatterns = [
     url("^%s/" % settings.EVENT_SLUG, include("mezzanine_agenda.urls")),
+    url("^%s/confirmation/(?P<transaction_id>[0-9]*)$" % settings.EVENT_SLUG, ConfirmationView.as_view(), name="organization-agenda-confirmation"),
 ]
+#
