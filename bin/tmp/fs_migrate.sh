9bed1a56e49e53cd4f1b6d116298c48586fd395c
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-04 23:46:27 +0100

    Mv scripts to bin

--- bin/tmp/fs_migrate.sh
+++ bin/tmp/fs_migrate.sh
@@ -7,8 +7,3 @@ sudo mv var/postgresql var/lib
 sudo mv var/external var/opt
 sudo mv var/var/log var
 sudo rm -rf var/var
-mkdir etc/nginx
-mkdir etc/nginx/conf.d/
-mv etc/nginx.conf etc/nginx/conf.d/default.conf
-mkdir etc/apt
-mv etc/sources.list etc/apt/
