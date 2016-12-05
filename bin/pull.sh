edd25edf6968c71be57d9e01852167666c8f577e
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-05 10:36:13 +0100

    Fix db restore path

--- bin/pull.sh
+++ bin/pull.sh
@@ -4,5 +4,5 @@ sudo chown -R $USER var/media
 sudo chown -R $USER var/backup
 git pull
 git submodule foreach git pull
-docker-compose run db /srv/bin/restore_db.sh
+docker-compose run db /srv/bin/db/restore.sh
 gulp build
