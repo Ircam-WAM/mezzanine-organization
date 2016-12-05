2aea391f1a3dc089ab747d39ad50f526b3f170f4
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-05 10:45:16 +0100

    Fix path, add doc build to upgrade

--- bin/db/backup.sh
+++ bin/db/backup.sh
@@ -2,6 +2,6 @@
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-pg_dump -Fc -hdb -Upostgres -dpostgres > /srv/backup/ircam-www.dump
+pg_dump -Fc -hdb -Upostgres -dpostgres > /srv/backup/postgres.dump
 
 echo "Backup done!"
