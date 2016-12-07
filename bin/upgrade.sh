0718280bcbdae8138594d40643af4b45dfba75f8
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-07 10:59:45 +0100

    Add bower allow-root option

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -4,6 +4,6 @@ git pull
 git submodule foreach git pull
 docker-compose run app python /srv/app/manage.py migrate
 # docker-compose run app python /srv/app/manage.py update_translation_fields
-docker-compose run app bash -c "cd /srv && bower install && gulp build"
+docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
 docker-compose run app python /srv/app/manage.py collectstatic --noinput
 docker-compose run app bash /srv/doc/build.sh
