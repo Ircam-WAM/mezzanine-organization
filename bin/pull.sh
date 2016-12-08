5ae04ace51299515844ad0bcd2c0535cb4d7d5e1
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-08 18:11:21 +0100

    Fix price==0 but reserve, de-ignore var (!)

--- bin/pull.sh
+++ bin/pull.sh
@@ -3,6 +3,6 @@
 sudo chown -R $USER var/media
 sudo chown -R $USER var/backup
 git pull
-git submodule foreach git pull
+git submodule foreach git pull origin master
 docker-compose run db /srv/bin/db/restore.sh
 docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
