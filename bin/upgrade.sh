2507cb836fc61bf8ba1dc6b8ead6d89bc1122a46
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-12-08 15:07:07 +0100

    Add touch app/wsgi.py to upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -7,3 +7,4 @@ docker-compose run app python /srv/app/manage.py migrate
 docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
 docker-compose run app python /srv/app/manage.py collectstatic --noinput
 docker-compose run app bash /srv/doc/build.sh
+touch app/wsgi.py
