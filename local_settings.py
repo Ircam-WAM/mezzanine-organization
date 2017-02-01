8b85e6ccab8e16452017b27fc13291d666d5c02c
Emilie Zawadzki <zawadzki@ircam.fr>
2017-02-01 18:02:12 +0100

    [Auth] : add LDAP

--- local_settings.py
+++ local_settings.py
@@ -22,6 +22,8 @@
 import os
 from django.utils.translation import ugettext_lazy as _
 from datetime import datetime, date
+import ldap, logging
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
 DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
@@ -273,8 +275,70 @@ FORMAT_MODULE_PATH = [
 FIGGO_API_URL_PROD='https://ircam.ilucca.net/'
 FIGGO_API_HEADER_AUTH='Lucca application=bd6d5481-40eb-414b-9135-434e12749223'
 
+##############################################
+##########  AUTHENTIFICATION LDAP  ###########
+##############################################
+# logging
+if DEBUG:
+    logger = logging.getLogger('django_auth_ldap')
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.DEBUG)
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = "ldap://nsldap.ircam.fr"
+# AUTH_LDAP_BIND_DN = "dc=ircam,dc=fr"
+# AUTH_LDAP_BIND_PASSWORD = ""
+# AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=ircam,dc=fr", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+
+# or perhaps:
+AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,ou=People,dc=ircam,dc=fr"
+
+# Set up the basic group parameters.
+# AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=django,ou=groups,dc=example,dc=com",
+#     ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)"
+# )
+# AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
+
+# Simple group restrictions
+# AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
+# AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
+
+# Populate the Django user from the LDAP directory.
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+}
+
+# AUTH_LDAP_PROFILE_ATTR_MAP = {
+#     "employee_number": "employeeNumber"
+# }
+#
+# AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+#     "is_active": "cn=active,ou=django,ou=groups,dc=example,dc=com",
+#     "is_staff": "cn=staff,ou=django,ou=groups,dc=example,dc=com",
+#     "is_superuser": "cn=superuser,ou=django,ou=groups,dc=example,dc=com"
+# }
+#
+# AUTH_LDAP_PROFILE_FLAGS_BY_GROUP = {
+#     "is_awesome": "cn=awesome,ou=django,ou=groups,dc=example,dc=com",
+# }
+
+# This is the default, but I like to be explicit.
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+
+# Use LDAP group membership to calculate group permissions.
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+# Cache group memberships for an hour to minimize LDAP traffic
+AUTH_LDAP_CACHE_GROUPS = True
+AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
+
+
+
 AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
     "mezzanine.core.auth_backends.MezzanineBackend",
-    'backends.IMAPBackend',
+    # 'backends.IMAPBackend',
     'guardian.backends.ObjectPermissionBackend',
 )
