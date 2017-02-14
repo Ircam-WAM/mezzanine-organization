783cf1f94c32e65a8f1d6a4203cef26524720fa6
Jérémy Fabre <Jeremy@iMac-de-Jeremy-2.fritz.box>
2017-02-14 10:24:51 +0100

    Update Instagram feed with a slider

--- static/src/js/modules/instagram.js
+++ static/src/js/modules/instagram.js
@@ -13,6 +13,28 @@ Instagram.prototype.init = function() {
 
     var that = this;
 
+    $('.instagram').lightSlider({
+        item: 3,
+        slideMargin: 0,
+        pager: false,
+        loop: true,
+        auto: true,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 752,
+                settings: {
+                    item: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    item: 1
+                }
+            }
+        ]
+    });
 
 };
 
