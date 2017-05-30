1cc0b68d798ec8ced776c9bc812c76817f1b9250
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-05-30 18:08:59 +0200

    Bugfix on context

--- organization/core/context_processors.py
+++ organization/core/context_processors.py
@@ -60,6 +60,6 @@ def organization_settings(request):
             'linked_organization_footer' : linked_org_footer,
             'linked_organization_footer_2' : linked_org_footer_2,
             'research_slug' : research_slug,
-            'menu_person_id': settings.MENU_PERSON_ID
+            'menu_person_id': settings.MENU_PERSON_ID,
             'debug_mode' : settings.DEBUG
             }
