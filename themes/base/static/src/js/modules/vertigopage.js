0d451bf21dcede0c5a62c464174dee1e7cfa1923
Sergio Martinez Losa <s.martinez@libelium.com>
2017-07-21 12:33:36 +0200

    [VERTIGO] Template page for vertigo index

--- themes/base/static/src/js/modules/vertigopage.js
+++ themes/base/static/src/js/modules/vertigopage.js
@@ -1,21 +1,31 @@
-
-function clickedLinkVertigo(link,event,title) {
+function clickedLinkVertigo(link,event) {
 
     event.preventDefault();
 
-    var buttons = document.getElementsByClassName('vertigopage__sibling');
+    var buttons = document.getElementsByClassName('vertigopage__button');
 
     for (var i = 0; i < buttons.length; i++)
     {
+        var indexItem = i + 1;
         var buttonLink = buttons[i];
 
-        buttonLink.style.display = 'none';
-        buttonLink.classList.remove('active');
+        buttonLink.classList.remove('vertigopage__selected');
+        buttonLink.classList.remove('vertigopage__unselected');
 
-        if (buttonLink.text === title)
+        var contentText = document.getElementById("content-" + indexItem);
+
+        contentText.classList.remove('vertigopage__visible');
+        contentText.classList.remove('vertigopage__invisible');
+
+        if (link.id === "link-" + indexItem)
         {
-            buttonLink.style.display = 'block';
-            buttonLink.classList.add('active');
+            buttonLink.classList.add('vertigopage__selected');
+            contentText.classList.add('vertigopage__visible');
+        }
+        else
+        {
+            buttonLink.classList.add('vertigopage__unselected');
+            contentText.classList.add('vertigopage__invisible');
         }
     }
 }
