8d774589d886737f97888df63480fa15041382e5
Sergio Martinez-Losa <s.martinez@libelium.com>
2018-01-12 12:31:37 +0100

    view revision for producers

--- organization/network/views.py
+++ organization/network/views.py
@@ -499,17 +499,17 @@ class ProducerListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(ProducerListView, self).get_context_data(**kwargs)
         context["description"] = ""
-        jury_list = PersonListBlock.objects.filter(title__in=["Jury", "jury"])
-        if jury_list:
-            jury = jury_list[0]
-            context["description"] = jury.description
-        context.update(self.kwargs)
+        producers_list = PersonListBlock.objects.filter(title__in=["Jury", "jury"])
+        if producers_list:
+            producer = producer_list[0]
+            context["description"] = producer.description
+            context.update(self.kwargs)
         return context
 
     def get_queryset(self):
-        jury_list = PersonListBlock.objects.filter(title__in=["Jury", "jury"])
-        if jury_list:
-            jury = jury_list[0]
-            qs = Person.objects.filter(person_list_block_inlines__person_list_block=jury).order_by("last_name")
+        producers_list = PersonListBlock.objects.filter(title__in=["Jury", "jury"])
+        if producers_list:
+            producer = producers_list[0]
+            qs = Person.objects.filter(person_list_block_inlines__person_list_block=producer).order_by("last_name")
         else:
-            qs = Person.objects.none()
+            qs = Person.objects.name
