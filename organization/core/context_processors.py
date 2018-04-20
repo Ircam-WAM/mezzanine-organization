85725c211194fb6cca4dc4cd1f7b138937329fc9
Emilie <zawadzki@ircam.fr>
2018-04-20 15:09:04 +0200

    [core] : default http_host for tests

--- organization/core/context_processors.py
+++ organization/core/context_processors.py
@@ -77,5 +77,5 @@ def organization_settings(request):
             'research_slug' : research_slug,
             'menu_person_id': settings.MENU_PERSON_ID,
             'debug_mode' : settings.DEBUG,
-            'http_host' :  request.environ['HTTP_HOST']
+            'http_host' :  request.environ['HTTP_HOST'] if 'HTTP_HOST' in request.environ else ''
             }
