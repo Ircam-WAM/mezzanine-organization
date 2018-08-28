decd04eabb9ec5b4563d3ec651b91c23d392d933
Emilie <zawadzki@ircam.fr>
2018-08-28 11:25:26 +0200

    [STMS Task #4] : functions

--- organization/core/utils.py
+++ organization/core/utils.py
@@ -1,3 +1,7 @@
+from django.contrib.sites.models import Site
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
 def split_events_from_other_related_content(context, related_content):
     context["related"] = {}
     context["related"]["other"] = []
@@ -8,3 +12,17 @@ def split_events_from_other_related_content(context, related_content):
         else :
             context["related"]["other"].append(rc)
     return context        
+
+
+def get_other_sites():
+    return Site.objects.exclude(pk=settings.SITE_ID)
+
+
+def actions_to_duplicate():
+    sites = get_other_sites()
+    actions = []
+    for site in sites:
+        actions.append('duplicate_content_to_' + site.domain.replace(".", "_"))
+    return actions
+
+
