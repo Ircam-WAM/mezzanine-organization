a5e20a0b66e99f4ea74bc1c26a93e3ee211abcdd
Emilie Zawadzki <emilie.zawadzki@ircam.fr>
2020-06-09 17:21:24 +0200

    [WWW Ticket 803] : increase DATA_UPLOAD_MAX_NUMBER_FIELDS

--- organization/settings.py
+++ organization/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
-SITE_ID = 5
+SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -234,7 +234,7 @@ DATABASES = {
 ################
 
 INSTALLED_APPS = [
-    # 'ircam_www_theme',
+    'ircam_www_theme',
     # the current theme has to be defined in main local_settings as HOST_THEMES
     "modeltranslation",
     "dal",
@@ -722,3 +722,4 @@ OWNABLE_MODELS_ALL_EDITABLE = []
 # ARTICLE LIST
 ARTICLE_KEYWORDS = ['', ]
 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
