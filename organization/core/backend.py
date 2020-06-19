51dfa6516fdee104bd85dc856da025a6c9a3da34
Emilie Zawadzki <emilie.zawadzki@ircam.fr>
2020-06-19 16:46:57 +0200

    [Manifeste Ticket #12] : add ALLOW_PERSON_CREATION

--- organization/core/backend.py
+++ organization/core/backend.py
@@ -10,33 +10,29 @@ class OrganizationLDAPBackend(LDAPBackend):
     def get_user(self, user_id):
         # a default created Person is assigned to the user
         user = super().get_user(user_id)
-        if user:
-            try :
-                # get Person by mail
-                person, created = Person.objects.get_or_create(email__iexact=user.email)
-                
-                person.user = user
-                # if the Person is the created default one
-                # if not user.person.first_name and not user.person.last_name:
-                    # old_person = user.person
-                    # reassign the right Person
-                    # delete the old one
-                    # old_person.delete()
 
-                # if a Person is created, populate it
-                if created:
-                    person.title=user.first_name+" "+user.last_name
-                    person.first_name=user.first_name
-                    person.last_name=user.last_name
-                    person.email=user.email
+        if getattr(settings, 'ALLOW_PERSON_CREATION', True):
+            if user:
+                try :
+                    # get Person by mail
+                    person, created = Person.objects.get_or_create(email__iexact=user.email)
 
-                person.save()
+                    person.user = user
 
-            except ObjectDoesNotExist:
-                person = Person.objects.get(
-                    email=user.email
-                )
-            except IntegrityError:
-                pass
+                    # if a Person is created, populate it
+                    if created:
+                        person.title=user.first_name+" "+user.last_name
+                        person.first_name=user.first_name
+                        person.last_name=user.last_name
+                        person.email=user.email
+
+                    person.save()
+
+                except ObjectDoesNotExist:
+                    person = Person.objects.get(
+                        email=user.email
+                    )
+                except IntegrityError:
+                    pass
 
         return user
