ce119040af5c6616d841182fe1ecca1ac7879db8
Clément Gérard <clement.gerard@ircam.fr>
2021-09-17 17:49:46 +0200

    minor fix

--- organization/agenda/migrations/0035_auto_20210628_1238.py
+++ organization/agenda/migrations/0035_auto_20210628_1238.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('organization-agenda', '0034_auto_20190130_1553'),
+        ('organization_agenda', '0034_auto_20190130_1553'),
     ]
 
     operations = [
