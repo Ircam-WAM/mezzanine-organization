989c783c2e7a9f4afba4cf7ecb0002122087957a
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-11-26 15:46:14 +0100

    update email command

--- organization/network/management/commands/organization-send-email-to-all-users.py
+++ organization/network/management/commands/organization-send-email-to-all-users.py
@@ -24,23 +24,39 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
-
+import time
 
 class Command(BaseCommand):
 
     help = "send an email to all users"
-    self.sender = 'no-reply@starts.eu'
+
+    def add_arguments(self, parser):
+        parser.add_argument('args', nargs='*')
 
     def handle(self, *args, **options):
-        subject = args[0]
+        subject = args[0] 
         html_email_path = args[1]
+
+        subject = settings.EMAIL_SUBJECT_PREFIX + ' ' + subject
+        sender = settings.DEFAULT_FROM_EMAIL
 
         f = open(html_email_path, 'r')
         html_message = f.read()
         f.close()
 
-        user_emails = [ user.email for user in User.objects.all() if user.email ]
+        emails = [ user.email for user in User.objects.filter(is_active=True) 
+                if user.email and not 'example.com' in user.email ]
+        #user_emails = ['pellerin@ircam.fr', 'pellerin@parisson.com']
+        print(len(emails))
 
-        message = EmailMessage(subject, html_message, 'no-reply@starts.eu', [], user_emails)
-        message.content_subtype = 'html'
-        message.send()
+        chunk_size = 80
+        chunks = [emails[x:x+chunk_size] for x in range(0, len(emails), chunk_size)]
+        
+        print(len(chunks))
+
+        for chunk in chunks:
+            print(len(chunk), chunk)
+            message = EmailMessage(subject, html_message, sender, [], chunk,)
+            message.content_subtype = 'html'
+            message.send()
+            time.sleep(10)
