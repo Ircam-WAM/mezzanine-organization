59c4d1cdf14b727720302c1f818ad3667ec8fe73
gllmp <guillaume.piccarreta@ircam.fr>
2022-10-26 11:13:31 +0200

    add project configuration fields to migrate command

--- organization/projects/management/commands/migrate-projects-configuration.py
+++ organization/projects/management/commands/migrate-projects-configuration.py
@@ -46,7 +46,16 @@ class Command(BaseCommand):
             json_conf = project.configuration
 
             project.version = json_conf['version']
-            #...
+            project.custom_link_url = json_conf['download_behavior']['strategies']['custom_links']['links']
+            project.git_tag = json_conf['download_behavior']['strategies']['repo_release']['git_tag']
+            project.include_sources = json_conf['download_behavior']['strategies']['repo_release']['include_sources']
+            project.include_binaries = json_conf['download_behavior']['strategies']['repo_release']['include_binaries']
+            project.git_ref_archive = json_conf['download_behavior']['strategies']['git_ref_archive']['git_ref']
+            project.project_release_ref = json_conf['download_behavior']['strategies']['project_release']['ref']
+            project.active_strategy = json_conf['download_behavior']['active_strategy']
+            project.is_protected = json_conf['global_asset_meta']['protected']
+            project.protection_endpoint = json_conf['global_asset_meta']['protection_endpoint']
+            project.protection_unlock_url = json_conf['global_asset_meta']['protection_unlock_url']
 
             if not dry_run:
                 project.save()
